6dbf1b102bd32dca2dfc7dade596324ff13b3226
Daniel Medrano <judamedranoba@ittepic.edu.mx>
2016-01-28 02:46:05 -0700

    Ejecutable Hola

--- Hola.java
+++ Hola.java
@@ -1,6 +1,6 @@
-public class Hola.java {
-	
-public static void main(String arg[]){
-	System.out.println("Hola Mundo");
-}	
+public class Hola {
+	//Imprime Hola
+       public static void main(String arg[]){
+		System.out.println("Hola Mundo");
+       }	
 }
